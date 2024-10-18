2ea5b99593d7c389ba140f93a3ec472ef409059a
Ady-cod <113712378+Ady-cod@users.noreply.github.com>
2024-10-18 14:57:20 +0300

    refactor: update Prisma Client singleton pattern and backend scripts for better environment handling and clean build process

--- hrms-backend-expressjs/prisma/client.ts
+++ hrms-backend-expressjs/prisma/client.ts
@@ -1,10 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 
-
-// Prevent multiple instances of Prisma Client in development
-if (!global.prisma) {
-  global.prisma = new PrismaClient();
+interface CustomNodeJsGlobal {
+  prisma: PrismaClient | undefined;
 }
-const prisma = global.prisma;
+
+declare const global: CustomNodeJsGlobal;
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
 export default prisma;
--- hrms-backend-expressjs/src/server.ts
+++ hrms-backend-expressjs/src/server.ts
@@ -14,5 +14,6 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.listen(PORT, () => {
+  console.log(`Server is working in the ${process.env.NODE_ENV} mode`);
   console.log(`Server is running at http://localhost:${PORT}`);
 });

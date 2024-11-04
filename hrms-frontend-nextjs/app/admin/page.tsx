5cf3eb23e98c31dc7eebd551a862dd1911184dd4
Sakshijagtap-27 <21104082.sakshi.jagtap@gmail.com>
2024-11-04 20:41:23 +0530

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch sakshi
     Your branch is up to date with 'origin/sakshi'.
    	new file:   pnpm-lock.yaml
    
     Changes to be committed:
    	modified:   hrms-frontend-nextjs/app/admin/page.tsx
    	new file:   hrms-frontend-nextjs/app/components/addbutton/AddNewDataButton.css
    	new file:   hrms-frontend-nextjs/app/components/addbutton/AddNewDataButton.tsx
    	new file:   package.json

--- hrms-frontend-nextjs/app/admin/page.tsx
+++ hrms-frontend-nextjs/app/admin/page.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import NavBar from '../components/navBar/navBar'
+// src/pages/page.tsx
+"use client";
+import React from 'react';
+import NavBar from '../components/navBar/navBar';
+import AddNewDataButton from '../components/addbutton/AddNewDataButton';
 
-const page = () => {
+const Page: React.FC = () => {
+    const handleAddNewDataClick = () => {
+        console.log("Add New Data button clicked");
+        // Implement any functionality you need when this button is clicked,
+        // like opening a modal or redirecting to a new page.
+    };
+
     return (
         <div>
-           <div> <NavBar/> </div>
+            <NavBar />
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '16px' }}>
+                <h2>Lists</h2>
+                <AddNewDataButton onClick={handleAddNewDataClick} />
+            </div>
+            {/* Other content like your table goes here */}
         </div>
-    )
-}
+    );
+};
 
-export default page
+export default Page;

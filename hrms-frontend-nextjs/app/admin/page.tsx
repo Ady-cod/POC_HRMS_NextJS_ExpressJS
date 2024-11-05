d450943b05395c9dfd1913e55d18a5258bcf6622
krishna26khatri <krishnakhatri565@gmail.com>
2024-11-05 15:18:49 +0530

    add sidebar with icons

--- hrms-frontend-nextjs/app/admin/page.tsx
+++ hrms-frontend-nextjs/app/admin/page.tsx
@@ -1,6 +1,14 @@
-import React from 'react'
+"use client"
+import React,{useState} from 'react'
 import NavBar from '../components/navBar/navBar'
+import SideBar from '../components/sidebar/sidebar'
 
+const Page=()=>{
+    const [isSideBarOpen,setIsSideBarOpen] = useState<boolean>(true)
+
+    const toggleSideBar=()=>{
+setIsSideBarOpen(!isSideBarOpen)
+    }
     return (
         <div>
             <NavBar isSideBarOpen={isSideBarOpen} toggleSideBar={toggleSideBar} />

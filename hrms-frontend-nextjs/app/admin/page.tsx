7e59fbfb0abc6f222439dd575038d2d66f2aa832
Sakshijagtap-27 <21104082.sakshi.jagtap@gmail.com>
2024-11-05 18:46:40 +0530

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch sakshi
     Your branch is up to date with 'origin/sakshi'.
    
     Changes to be committed:

--- hrms-frontend-nextjs/app/admin/page.tsx
+++ hrms-frontend-nextjs/app/admin/page.tsx
@@ -1,13 +1,44 @@
-import React from 'react'
+"use client"
+import React,{useState} from 'react'
 import NavBar from '../components/navBar/navBar'
+import SideBar from '../components/sidebar/sidebar'
+import Footer from '../components/footer/footer'
+import AddNewDataButton from '../components/addbutton/AddNewDataButton';
+import ModalForm from '../components/addbutton/ModalForm';
+
+const Page=()=>{
+    const [isSideBarOpen,setIsSideBarOpen] = useState<boolean>(true)
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     const toggleSideBar=()=>{
 setIsSideBarOpen(!isSideBarOpen)
     }
+     // Open and close handlers for the modal
+     const handleAddNewDataClick = () => {
+        setIsModalOpen(true);
+    };
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    };
+
+
     return (
-        <>
+        <div>
             <NavBar isSideBarOpen={isSideBarOpen} toggleSideBar={toggleSideBar} />
             <SideBar isOpen={isSideBarOpen} />
+            {/* Header section with Add New Data button */}
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '16px' }}>
+                <h2>Lists</h2>
+                <AddNewDataButton onClick={handleAddNewDataClick} />
+            </div>
+
+            {/* Modal Form for adding new data */}
+            <ModalForm isOpen={isModalOpen} onClose={handleCloseModal} />
+            
+            {/* Other content like your table goes here */}
+        
+
+            <Footer/>
         </div>
     );
 };

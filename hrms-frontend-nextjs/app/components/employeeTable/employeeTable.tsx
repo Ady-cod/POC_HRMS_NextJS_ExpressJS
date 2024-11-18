a80f0e08d9293a73a1680787e7596f1f257a4958
Ady-cod <113712378+Ady-cod@users.noreply.github.com>
2024-11-18 02:12:44 +0200

    refactor(frontend): Implement getAllEmployees server action, add Employee-related types, and update component naming and path imports with alias

--- hrms-frontend-nextjs/app/components/employeeTable/employeeTable.tsx
+++ hrms-frontend-nextjs/app/components/employeeTable/employeeTable.tsx
@@ -1,29 +1,45 @@
 import React,{useState,useEffect} from 'react'
 import DataTable from 'react-data-table-component';
+import { getAllEmployees } from '@/actions/employee';
 
 
-const employeeTable = () => {
-    const [employee,setEmployee]= useState([])
+const EmployeeTable = () => {
+  
+  // const employees = await getAllEmployees();
 
-    useEffect(() => {
-        // Fetch data from the backend
-        const fetchEmployees = async () => {
-          try {           
-            const response = await fetch('http://localhost:5000/api/v1/employee',{
-              method:'GET'
-            });
-            const data = await response.json();
-        console.log(data)
-           setEmployee(data);
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    async function fetchEmployees() {
+      const employees = await getAllEmployees();
+      setEmployees(employees);
+    }
+    fetchEmployees();
+  }, []);
+
+
+
+    // const [employees,setEmployees]= useState([])
+
+    // useEffect(() => {
+    //     // Fetch data from the backend
+    //     const fetchEmployees = async () => {
+    //       try {           
+    //         const response = await fetch('http://localhost:5000/api/v1/employee',{
+    //           method:'GET'
+    //         });
+    //         const data = await response.json();
+    //     console.log(data)
+    //        setEmployees(data);
           
-           // console.log(users)
-          } catch (error) {
-            console.error('Error fetching data:', error);
-          }
-        }
+    //        // console.log(users)
+    //       } catch (error) {
+    //         console.error('Error fetching data:', error);
+    //       }
+    //     }
     
-        fetchEmployees();
-      },[]);
+    //     fetchEmployees();
+    //   },[]);
     
     const columns = [
         {
@@ -34,22 +50,22 @@ const employeeTable = () => {
         },
         {
             name: 'First Name',
-            selector: row => row.firstName,
+            selector: row => row.firstName || 'N/A',
             sortable: true
         },
         {
             name: 'Email',
-            selector: row => row.email,
+            selector: row => row.email || 'N/A',
             sortable: true
         },
         {
             name: 'Status',
-            selector: row => row.status,
+            selector: row => row.status || 'N/A',
             sortable: true
         },
         {
             name: 'Role',
-            selector: row => row.role,
+            selector: row => row.role || 'N/A',
             sortable: true
         },
         {
@@ -58,17 +74,17 @@ const employeeTable = () => {
             sortable:true,
             cell: row => (
                <div>
-                <button onClick={()=>handleEdit(row)} className="bg-green-700 rounded-lg p-2">Edit</button>
-                <button onClick={()=>{handleDelete(row)}} className="bg-red-700  rounded-lg p-2 ms-2">Delete</button>
+                <button onClick={()=>handleEdit(row)} className="bg-green-500 rounded-lg p-2">Edit</button>
+                <button onClick={()=>{handleDelete(row)}} className="bg-red-500  rounded-lg p-2 ms-2">Delete</button>
               </div>
             ),
           }
     ];
     return (
-        <div>
-         <DataTable columns={columns} data ={employee}/>
-        </div>
-    )
+      <div>
+        <DataTable columns={columns} data={employees} />
+      </div>
+    );
 }
 
-export default employeeTable;
+export default EmployeeTable;

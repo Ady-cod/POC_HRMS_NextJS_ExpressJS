90134a46e6420fcaa0f8ba637d739981eacee44e
flash <email2sd.com>
2025-01-15 04:19:30 +0530

    feat(Sidebar): Integrate ShadCN Sheet for small-screen sidebar and made it a client component to use useeffect
    - Added ShadCN Sheet component to handle sidebar rendering on smaller screens
    - Created `smallScreenSidebar` function to encapsulate Sheet logic and rednder it on small screens
    - Configured Sheet with customizable width on smaller screens
    - Set default width for small screens and added adjustments for specific breakpoints
    - Added `isSmBreakpoint` state to toggle between small and large screen layouts
    - Created isSheetOpen (useState variable) to toggle sheet(sidebar)
    - Used `window.matchMedia` to detect screen size dynamically
    - Included `useEffect` to handle changes in screen size during runtime
    - Ensured cleanup of event listeners on component unmount
    - Prevented state conflicts between the parent component and the Sheet
    -  Introduced `handleSheetOpenChange` to manage Sheet's open/close state.
    - Ensured `isSheetOpen` state reflects changes in the `isOpen` prop

--- hrms-frontend-nextjs/app/components/Sidebar/Sidebar.tsx
+++ hrms-frontend-nextjs/app/components/Sidebar/Sidebar.tsx
@@ -1,16 +1,88 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import "./Sidebar.css";
+import { useEffect, useState, useRef } from "react";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { boolean } from "zod";
 
 interface SideBarProps {
   isOpen: boolean;
+  toggleSidebar: (value: boolean) => void;
 }
 
-const Sidebar = ({ isOpen }: SideBarProps) => {
-    
-  return (
-    <>
+const Sidebar = ({ isOpen, toggleSidebar }: SideBarProps) => {
+  const [isSmBreakpoint, setIsSmBreakpoint] = useState(true);
+  const [isSheetOpen, setIsSheetOpen] = useState(false);
+
+  useEffect(() => {
+
+    // Media query for 'sm' breakpoint
+    const mediaQuery = window.matchMedia("(min-width: 640px)");
+
+    // Initial state based on the current match
+    setIsSmBreakpoint(mediaQuery.matches);
+
+    // Event listener for changes to the media query
+    const handleMediaQueryChange = (e: MediaQueryListEvent) => {
+      setIsSmBreakpoint(e.matches);
+    };
+
+    // Listen for changes
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Cleanup listener on component unmount
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  const handleSheetOpenChange = (open: boolean) => {
+    setIsSheetOpen(open);
+    if (!open) {
+      toggleSidebar(false); // Set isOpen to false when the sheet is closed
+    }
+  };
+
+
+  useEffect(() => {
+    if(isOpen){
+      setIsSheetOpen(true)
+    }
+    else if (!isOpen) {
+      setIsSheetOpen(false);
+    }
+  }, [isOpen,isSheetOpen])
+  
+
+  const smallScreenSidebar = () => {
+    return (
+      <Sheet open={isSheetOpen} onOpenChange={handleSheetOpenChange}>
+        <SheetTrigger ></SheetTrigger>
+        <SheetContent className="w-[50%] max-[400px]:w-[65%] p-0 max-[525px]:pl-4 pl-8 pt-12" side="left">
+          <SheetHeader>
+            <SheetTitle>
+              {renderSidebar()}
+             
+            </SheetTitle>
+           
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    );
+  };
+
+  const renderSidebar = () => {
+    return (
       <div
         className={`transition-transform-500 ${
           isOpen ? "transform-translate-x-0" : "transform -translate-x-full hidden"

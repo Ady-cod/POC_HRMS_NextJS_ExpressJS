a1fa575ad04b76bb93f0ab5c38217e7d755f359c
Souvik <souvik.basak2404@gmail.com>
2025-07-14 16:18:07 +0530

    Update DailyQuote Component
    - Update DailyQuote Component by removing IBM Plex Sans Font.

--- hrms-frontend-nextjs/app/components/DailyQuote/DailyQuote.tsx
+++ hrms-frontend-nextjs/app/components/DailyQuote/DailyQuote.tsx
@@ -2,14 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { Card } from "@/components/ui/card";
-import { IBM_Plex_Sans } from "next/font/google";
 import { fetchDailyQuote } from "@/lib/quotes";
-
-const ibmPlexSans = IBM_Plex_Sans({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  variable: "--font-ibm-plex-sans",
-});
 
 const DailyQuote = () => {
   const [quote, setQuote] = useState<{ quote: string; name: string }>({
@@ -34,10 +27,10 @@ const DailyQuote = () => {
 
   return (
     <Card
-      className={`p-6 text-center bg-black/10 ${ibmPlexSans.variable} text-xl md:text-2xl lg:text-3xl xl:text-4xl`}
+      className={`p-6 text-center bg-black/10  text-xl md:text-2xl lg:text-3xl xl:text-4xl`}
     >
       <h2 className="font-bold">Today&apos;s Quote</h2>
-      <p className="text-gray-700 mt-4">&ldquo;{quote.quote}&rdquo;</p>
+      <p className="text-gray-500 mt-4 font-bold">&ldquo;{quote.quote}&rdquo;</p>
       <p className="text-gray-500 mt-2 text-lg md:text-xl lg:text-2xl xl:text-3xl text-end">
         – {quote.name}
       </p>

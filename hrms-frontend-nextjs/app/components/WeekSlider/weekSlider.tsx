757a30e3f0a6e99510d339c0e39e97c2ac5e057d
Ady-cod <113712378+Ady-cod@users.noreply.github.com>
2025-07-30 17:17:33 +0300

    fix(WeekSlider): adjust import for WeekSlider.css using Pascal Case

--- hrms-frontend-nextjs/app/components/WeekSlider/weekSlider.tsx
+++ hrms-frontend-nextjs/app/components/WeekSlider/weekSlider.tsx
@@ -4,7 +4,7 @@ import React, { useRef, useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretLeft, faCaretRight } from "@fortawesome/free-solid-svg-icons";
 
-import "./weekSlider.css";
+import "./WeekSlider.css";
 
 /**
  * WeekSlider component with horizontal scrolling navigation.

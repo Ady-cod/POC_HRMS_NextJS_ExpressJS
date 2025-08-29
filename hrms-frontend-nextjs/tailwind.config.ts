5586bb8ab5dc15e38556f36cb59a7d63baa28569
Souvik <souvik.basak2404@gmail.com>
2025-08-29 13:46:45 +0530

    feat(tailwind): add custom color palettes for lightblue, darkblue, and orange
    - Extend Tailwind configuration with new color palettes
    - Define lightblue, darkblue, and orange shades for consistent use
    - Enable easier theming and brand alignment across UI components

--- hrms-frontend-nextjs/tailwind.config.ts
+++ hrms-frontend-nextjs/tailwind.config.ts
@@ -55,6 +55,44 @@ const config: Config = {
           "4": "hsl(var(--chart-4))",
           "5": "hsl(var(--chart-5))",
         },
+
+        // Custom Palettes
+        lightblue: {
+          50: "#E6F4F9",
+          100: "#B0DCEE",
+          200: "#8ACBE5",
+          300: "#54B3D9",
+          400: "#33A5D2",
+          500: "#008EC7",
+          600: "#0081B5",
+          700: "#00658D",
+          800: "#004E6D",
+          900: "#003C54",
+        },
+        darkblue: {
+          50: "#E7ECF0",
+          100: "#B4C3D0",
+          200: "#8FA6B9",
+          300: "#5C7E98",
+          400: "#3D6585",
+          500: "#0C3E66",
+          600: "#0B385D",
+          700: "#092C48",
+          800: "#072238",
+          900: "#051A2B",
+        },
+        orange: {
+          50: "#FDF1EA",
+          100: "#F8D3BC",
+          200: "#F5BE9C",
+          300: "#F0A06F",
+          400: "#ED8D53",
+          500: "#E97128",
+          600: "#D46724",
+          700: "#A5501C",
+          800: "#803E16",
+          900: "#622F11",
+        },
       },
       borderRadius: {
         lg: "var(--radius)",
@@ -65,4 +103,5 @@ const config: Config = {
   },
   plugins: [tailwindcssAnimate],
 };
+
 export default config;
